acc75c641d9a123c5b7857342efbdc40b87336a1
Marc <zunly@hotmail.com>
2022-07-19 15:04:00 +0800

    第二个jni方法, 直接传参, 并return该形参的+1值.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,37 @@
 #include <jni.h>
 #include <string>
 
+#include <android/log.h>
+
+#define TAG "NATIVE_LIB"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__);
+
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_wulala_myjnidemo_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+Java_com_wulala_myjnidemo_MainActivity_stringFromJNI(JNIEnv *env, jobject) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+// 这个函数的意义就是修改Java对象内的String类型的field.
+extern "C" JNIEXPORT void JNICALL
+Java_com_wulala_myjnidemo_MainActivity_changeText(JNIEnv *env, jobject thiz) {
+
+    LOGD("Enter C .. \n");
+
+    jclass mainActivityClass = env->GetObjectClass(thiz);  // 第一步, 获取MainActivity类
+
+    jfieldID centerTextFid = env->GetFieldID(mainActivityClass, "centerText", "Ljava/lang/String;");   // 第二步, 获取centerText这个String对象的签名
+
+    jstring newValue = env->NewStringUTF("Oh No!");   // 第三步, 生成一个新的jstring对象
+
+    env->SetObjectField(thiz, centerTextFid, newValue);    // 第四步, 给java的field赋值
+
+    // 已测试可用
+}
+
+// 这个函数的意义是有形参, 把Java对象中的field传参进来, 然后同时有输出
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_wulala_myjnidemo_MainActivity_plusOne(JNIEnv *env, jobject thiz, jint number) {
+    return number + 1;    // 可见有传参的情况下, 无需那么多jni的调用.
 }
--- app/src/main/java/com/wulala/myjnidemo/MainActivity.java
+++ app/src/main/java/com/wulala/myjnidemo/MainActivity.java
@@ -2,20 +2,31 @@ package com.wulala.myjnidemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.wulala.myjnidemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'myjnidemo' library on application startup.
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     static {
         System.loadLibrary("myjnidemo");
     }
 
     private ActivityMainBinding binding;
+    TextView tv;
+    TextView numberTextView;
+    String centerText = "Oh yeah";
 
+    int numberTextNumber = 2;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +35,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
+        tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+        numberTextView = binding.numberText;
+        numberTextView.setText(numberTextNumber + "");
+
+        Button changeBtn = binding.changeBtn;
+        changeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onChangeBtnClick();
+            }
+        });
+
+        binding.plusOneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberTextNumber = plusOne(numberTextNumber);  // 重新赋值
+                binding.numberText.setText(numberTextNumber+ "");
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'myjnidemo' native library,
-     * which is packaged with this application.
-     */
+    public void onChangeBtnClick() {
+        Log.d(TAG, "onChangeBtnClick: yes!");
+        changeText();
+        tv.setText(centerText);
+    }
+
+    public native int plusOne(int number);
+
+    public native void changeText();
+
     public native String stringFromJNI();
+
 }
